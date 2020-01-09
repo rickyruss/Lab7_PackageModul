c798dbe4216215e17814ad0769e26a3f045e1f56
rickyruss <56240498+rickyruss@users.noreply.github.com>
2020-01-09 21:41:13 +0700

    Syntax Package dan Modul

---
+++ Syntax Modul package/main.py
@@ -0,0 +1,25 @@
+from view.view_nilai import *
+
+while True:
+    c = input("\nA)dd, E)dit, S)earch, D)elete L)ist, Q)uit: ")
+
+    if (c.lower() == 'a'):
+        tambah_data()
+
+    elif (c.lower() == 'e'):
+        ubah_data()
+
+    elif (c.lower() == 's'):
+        cetak_hasil_pencarian()
+
+    elif (c.lower() == 'd'):
+        hapus_data()
+
+    elif (c.lower() == 'l'):
+        cetak_daftar_nilai()
+
+    elif (c.lower() == 'q'):
+        break
+
+    else:
+        print("Pilih yang bener dong ah!")
---
+++ Syntax Modul package/view/view_nilai.py
@@ -0,0 +1,35 @@
+from model.daftar_nilai import *
+
+def cetak_daftar_nilai():
+    if dataMhs.items():
+        print("\n                      DAFTAR NILAI MAHASISWA                    ")
+        print("==================================================================")
+        print("| No |     Nama     |    NIM    | Tugas |  UTS  |  UAS  |  Akhir |")
+        print("==================================================================")
+        i = 0
+        for x in dataMhs.items():
+            i += 1
+            print("| {6:2} | {0:12s} | {1:9s} | {2:5} | {3:5} | {4:5} | {5:6} |".format(x[0], x[1][0], x[1][1], x[1][2],
+                                                                                        x[1][3], x[1][4], i))
+        print("==================================================================")
+    else:
+        print("\n                      DAFTAR NILAI MAHASISWA                    ")
+        print("==================================================================")
+        print("| No |     Nama     |    NIM    | Tugas |  UTS  |  UAS  |  Akhir |")
+        print("==================================================================")
+        print("|                          TIDAK ADA DATA!                       |")
+        print("==================================================================")
+
+def cetak_hasil_pencarian():
+    nama = input("Masukkan Nama        : ")
+    if nama in dataMhs.keys():
+        print("\n                   DAFTAR NILAI MAHASISWA                   ")
+        print("==============================================================")
+        print("|     Nama     |    NIM    | Tugas |  UTS  |  UAS  |  Akhir  |")
+        print("==============================================================")
+        print("| {0:12s} | {1:9s} | {2:5} | {3:5} | {4:5} | {5:6}  |".format(nama, dataMhs[nama][0], dataMhs[nama][1],
+                                                                            dataMhs[nama][2], dataMhs[nama][3],
+                                                                            dataMhs[nama][4]))
+        print("==============================================================")
+    else:
+        print("Datanya {0} Tidak Ada ".format(nama))
